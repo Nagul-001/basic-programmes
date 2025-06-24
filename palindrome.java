40124d6f1c6efa6d023b78b9640ce2193b4e41c5
Nagul-001 <crnagul7@gmail.com>
2025-06-24 10:14:34 +0530

    program to check string is a palindrome

--- palindrome.java
+++ palindrome.java
@@ -1,1 +1,18 @@
-public class palindrome {    public static void main(String[] args) {        System.out.println(isPalindrome("markram"));    }    static boolean isPalindrome(String str){        int start=0;        int end=str.length()-1;        while (start<=end){            if(str.charAt(start)!=str.charAt(end)){                return false;            }            start++;            end--;        }        return true;    }}
+public class palindrome {
+    public static void main(String[] args) {
+        boolean result=isPalindrome("markram");
+        System.out.println(result);
+    }
+    static boolean isPalindrome(String str){
+        int start=0;
+        int end=str.length()-1;
+        while (start<=end){
+            if(str.charAt(start)!=str.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
